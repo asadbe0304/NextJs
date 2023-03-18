4533c58faeaa22c806c2f3feb8be40f2ae35bdfa
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-18 15:36:50 +0500

    mui config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import type { AppProps } from 'next/app'
+import createEmotionCache from '@/src/helpers/create-emtion-cache'
+import { Roboto } from 'next/font/google'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '@/src/helpers/theme'
+import CssBaseline from '@mui/material/CssBaseline';const roboto = Roboto({
+  weight: ["300", '400', "500", '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (<>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </CacheProvider>
+  </>
+  )
+}
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import {Button} from "@mui/material"
 export default function Home() {
   return (
     <>
@@ -14,8 +9,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
+      <div>
         Home Page
+        <Button>
+        click
+        </Button>
       </div>
     </>
   )
